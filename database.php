71d6b45a9d06a724203093987057b9171d466fb5
Pravin Tripathi <pravin.tripathi@ves.ac.in>
2016-11-23 22:49:19 +0530

    Update database.php

--- database.php
+++ database.php
@@ -3,7 +3,9 @@ $DBServer='host url';
 $DBUser='Username';
 $DBPassword='password';
 $DBName='database name';
-
+/*
+* 	Create connection to the server
+*/
 $connect=new mysqli($DBServer,$DBUser,$DBPassword,$DBName);
 if($connect->connect_error)
 {
